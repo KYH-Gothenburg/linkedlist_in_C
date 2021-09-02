c7617e2bf26814091773efded5452b693c224a6c
Joakim Wassberg <joakim@arthead.se>
2021-09-02 09:48:21 +0200

    Added comments and tail

--- linkedlist1.c
+++ linkedlist1.c
@@ -14,20 +14,33 @@ void print_list(Node *node) {
     printf("\n");
 }
 
+// push end
+// pop end
+// push head
+// pop head
+// push at
+// pop at
+// pop value
+// Alla pop skall returna datat i den poppade noden
+
+// Gör om listan till en dubbellänkad lista
+
 int main() {
     Node *head = NULL;
+    Node *tail = NULL;
     Node *current = NULL;
 
     head = (Node *)malloc(sizeof(Node));
     head->data = 10;  // (*head).data
     head->next = NULL;
+    tail = head;
 
     current = (Node *)malloc(sizeof(Node));
     current->data = 11;
     current->next = NULL;
 
     head->next = current;
-
+    tail = current;
     print_list(head);
     
     current = head;
@@ -36,6 +49,7 @@ int main() {
         free(current);
         current = head;
     }
+    tail = NULL;
     if(head == NULL) {
         printf("List is empty!\n");
     }
